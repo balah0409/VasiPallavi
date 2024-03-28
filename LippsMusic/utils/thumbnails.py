f2ec92b352aa10424ffcb2e3870e6755a918c416
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-03-28 16:41:37 +0530

    Update thumbnails.py

--- LippsMusic/utils/thumbnails.py
+++ LippsMusic/utils/thumbnails.py
@@ -81,7 +81,7 @@ async def get_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
+            (5, 5), f"LS/{MUSIC_BOT_NAME}", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
