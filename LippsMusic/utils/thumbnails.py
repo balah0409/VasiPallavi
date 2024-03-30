1c8213f0b41cf544c315a0c1a12ff769952da81b
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-03-30 14:36:43 +0530

    Update thumbnails.py

--- LippsMusic/utils/thumbnails.py
+++ LippsMusic/utils/thumbnails.py
@@ -196,7 +196,8 @@ async def get_thumb(videoid: str):
         draw_text_with_shadow(background, draw, (text_x_position, 180), title1[0], title_font, (255, 255, 255))
         draw_text_with_shadow(background, draw, (text_x_position, 230), title1[1], title_font, (255, 255, 255))
         draw_text_with_shadow(background, draw, (text_x_position, 320), f"{channel}  |  {views[:23]}", arial, (255, 255, 255))
-        draw_text_with_shadow(background, draw, (text_x_position, 1), f"{MUSIC_BOT_NAME}" , arial, (255, 255, 255))
+        draw_text_with_shadow(background, draw, (text_x_position, (6,6)), f"{MUSIC_BOT_NAME}" , arial, (255, 255, 255))
+        
 
 
         line_length = 580  
