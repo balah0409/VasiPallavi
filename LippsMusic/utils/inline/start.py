cab408e80f6155b8fbabe541fb4f8b06e35db8ba
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-04-12 12:42:19 +0530

    Update start.py

--- LippsMusic/utils/inline/start.py
+++ LippsMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-           InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/Lippsxd/LippsMusicBot"),
+           InlineKeyboardButton(text=_["S_B_7"], url=f"https://telegra.ph/file/edc164d10e265626b00a4.jpg"),
         ],
     ]
     return buttons
