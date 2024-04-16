2a4e3cf9c15a5547ed3747acbf9c14956a2440e3
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-04-16 13:22:51 +0530

    Update play.py

--- LippsMusic/utils/inline/play.py
+++ LippsMusic/utils/inline/play.py
@@ -66,12 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),            
         ],
-        [
-            InlineKeyboardButton(
-                text="⇆ 𝐒ʜᴜғғʟᴇ ⇆",
-                callback_data=f"ADMIN Shuffle|{chat_id}"
-            ),
-        ],
+       
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
